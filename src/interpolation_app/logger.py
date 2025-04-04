4df740d04204b739fc095cf753b0834c8a2d1a26
Stimm147 <michalczewski0@gmail.com>
2025-04-04 15:29:36 +0200

    logger added

---
+++ src/interpolation_app/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+
+def get_logger(name: str = "frame_interpolation") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+            "%Y-%m-%d %H:%M:%S"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
